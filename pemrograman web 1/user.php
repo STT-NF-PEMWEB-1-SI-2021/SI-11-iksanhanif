6e4e978e719662d0fa90fd48c3659cfb1b4ed0ed
iksanhanif30 <92282271+iksanhanif30@users.noreply.github.com>
2022-03-07 21:50:37 +0700

    Add files via upload

---
+++ pemrograman web 1/user.php
@@ -0,0 +1,14 @@
+<?php
+// definisikan variable
+$nama = 'Iksan Hanif';
+$umur = 20;
+$berat = 50;
+
+
+echo 'Nama : ' .$nama;
+echo '<br/>umur : ' .$umur. 'kg';
+echo '<br/>Berat : '.$berat. 'kg';
+
+echo "<br/> hello $nama apa kabar?";
+echo "<br/> Hai Nama Saya $nama umur saya $umur Berat badan saya $berat kg, salam kenal semuanya";
+echo "<hr>";
